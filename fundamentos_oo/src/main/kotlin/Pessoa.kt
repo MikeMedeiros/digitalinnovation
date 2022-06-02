19293d52dea30b64c485d78af1582eefc35228ec
mike <mikaellmedeiros600@gmail.com>
2022-06-02 10:57:12 -0300

    created person

---
+++ fundamentos_oo/src/main/kotlin/Pessoa.kt
@@ -0,0 +1,11 @@
+class Pessoa {
+    var nome: String = "Mikael"
+    var cpf:  String = "021.021.042-14"
+
+}
+
+fun main(){
+    val mikael = Pessoa()
+    println(mikael.nome)
+    println(mikael.cpf)
+}
