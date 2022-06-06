47c86ecfcc39e5cbbb2f3850d609e728e8854db4
mike <mikaellmedeiros600@gmail.com>
2022-06-06 10:11:27 -0300

    create account class

---
+++ fundamentos_oo/src/main/kotlin/conta.kt
@@ -0,0 +1,17 @@
+package one.digitalinnovation.digionebank
+
+class conta (
+    val agencia: String,
+    val numero: String,
+    val saldo: Double
+){
+    fun deposito(valor:Double){
+
+    }
+    fun sacar(valor: Double){
+
+
+    }
+
+
+}
