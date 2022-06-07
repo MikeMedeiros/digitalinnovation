4bf939360c5c3f723751271a972d6ce510a3eb67
mike <mikaellmedeiros600@gmail.com>
2022-06-07 09:28:07 -0300

    created int Array test class

---
+++ src/main/kotlin/on/digitalinnovation/collections/TesteArray.kt
@@ -0,0 +1,16 @@
+package on.digitalinnovation.collections
+
+fun main() {
+    val values = IntArray(5)
+
+values[0] = 2
+    values[1] = 3
+    values[2] = 4
+    values[3] = 25
+    values[4] = 5
+
+for(valor in values){
+    println(valor)
+}
+
+}
