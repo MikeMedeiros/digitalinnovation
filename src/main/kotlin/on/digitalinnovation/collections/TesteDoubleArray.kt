04709d4436df979d509527ca44ddc6518164db67
mike <mikaellmedeiros600@gmail.com>
2022-06-07 10:07:37 -0300

    created double array

---
+++ src/main/kotlin/on/digitalinnovation/collections/TesteDoubleArray.kt
@@ -0,0 +1,17 @@
+package on.digitalinnovation.collections
+
+fun main() {
+
+        val salarios = DoubleArray(3)
+        salarios[0] = 1000.0
+        salarios[1] = 3000.0
+        salarios[2] = 500.0
+
+        salarios.forEach { println(it) }
+
+        println("-----------------------------------")
+        salarios.forEachIndexed { index, salario ->
+            salarios[index] = salario * 1.1
+        }
+        salarios.forEach { println(it) }
+}
